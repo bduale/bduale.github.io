3bbf4a84b550ee7ebcaae8d02b15344d71de5dd2
bduale <duale.bashir@hotmail.com>
2020-11-12 21:17:36 -0500

    updating modal js

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,36 @@
+function include(file) {
+
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
+
+}
+
 function openModal(e) {
-  console.log(e.target.id);
+  var project = findProject(e);
+  var project_location = '../bduale.github.io/projects/' + project.toString() + '/';
+  var project_js = project_location + project.toString() + '.js'
+
+  jQuery.getScript(project_js, function() {
+    var data = getData();
+    console.log(data);
+  });
+
   jQuery('#openModal').click();
   jQuery('.close-modal').hide();
+}
+
+function findProject(e) {
+  var project;
+  if(e.path[2].id != '')
+      project = e.path[2].id;
+  else if(e.path[4].id != '')
+      project = e.path[4].id;
+  else if(e.path[6].id != '')
+      project = e.path[6].id;
+
+  return project;
 }
---
+++ projects/march_madness/march_madness.js
@@ -0,0 +1,22 @@
+var title = 'NCAA Mens Basketball Predictor';
+
+var overview = '';
+
+var languages = '';
+
+var libraries = '';
+
+var frameworks = '';
+
+var link = '';
+
+var data = {'title': title,
+            'overview': overview,
+            'languages': languages,
+            'libraries': libraries,
+            'frameworks': frameworks,
+            'link': link};
+
+function getData() {
+  return data;
+}
